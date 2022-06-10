881addbf0cf7714068bc96245acd296ab2a54eec
Ivo Schwizer <ivoschwizer@gmail.com>
2022-06-10 15:02:07 +0200

    dk

--- head.php
+++ head.php
@@ -3,41 +3,49 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="robots" content="all">
 <meta name="robots" content="index, follow" />
-<meta name="author" content="Art of Loft AG">
+<meta name="author" content="Locher Food">
 
 <meta http-equiv="cache-control" content="max-age=0" />
 <meta http-equiv="cache-control" content="no-cache" />
 <meta http-equiv="expires" content="0" />
 <meta http-equiv="pragma" content="no-cache" />
 
-<meta property="og:title" content="Art of Loft" />
-<meta property="og:site_name" content="Art of Loft">
+<meta property="og:title" content="Locher Food" />
+<meta property="og:site_name" content="Locher Food">
 <meta property="og:type" content="Website">
-<meta property="og:description" content="Art of Loft AG - Architektur und Projektmanagement. Die andere Sicht der nachhaltigen Architektur unter Einbezug von Funktion, Reduktion und Ästhetik.">
-<meta property="og:url" content="https://artofloft.ch/">
-<meta property="og:image" content="https://artofloft.ch/img/share.jpg">
-<meta property="og:image:secure_url" content="https://artofloft.ch/img/share.jpg">
+<meta property="og:description" content="Locher Food and Drink - Wir sind Händler aus Passion.">
+<meta property="og:url" content="https://locherfood.ch/">
+<meta property="og:image" content="https://locherfood.ch/img/share.jpg">
+<meta property="og:image:secure_url" content="https://locherfood.ch/img/share.jpg">
 <meta property="og:image:type" content="image/jpeg">
 <meta property="og:image:width" content="1200">
 <meta property="og:image:height" content="630">
-<meta property="og:image:alt" content="Art of Loft">
+<meta property="og:image:alt" content="Locher Food">
 <meta name="twitter:card" content="summary_large_image">
-<meta name="twitter:title" content="Art of Loft">
-<meta name="twitter:description" content="Die Art of Loft AG St. Gallen zählt seit vielen Jahren zu den Akteuren der Architekturszene der Ostschweiz">
-<meta name="twitter:image" content="https://artofloft.ch/img/share.jpg">
-<meta name="twitter:image:alt" content="Art of Loft">
+<meta name="twitter:title" content="Locher Food">
+<meta name="twitter:description" content="Locher Food and Drink - Wir sind Händler aus Passion.">
+<meta name="twitter:image" content="https://locherfood.ch/img/share.jpg">
+<meta name="twitter:image:alt" content="Locher Food">
 <meta name="apple-mobile-web-app-capable" content="yes">
 <meta name="apple-mobile-web-app-status-bar-style" content="black">
-<meta name="apple-mobile-web-app-title" content="Art of Loft">
+<meta name="apple-mobile-web-app-title" content="Locher Food">
 <meta name="theme-color" content="#ffffff">
 <meta name="format-detection" content="telephone=yes">
-<meta property="business:contact_data:street_address" content="Schützengartenweg 3">
-<meta property="business:contact_data:locality" content="St.Gallen">
-<meta property="business:contact_data:region" content="9000">
-<meta property="business:contact_data:postal_code" content="9000">
+<meta property="business:contact_data:street_address" content="Wieslistrasse 15">
+<meta property="business:contact_data:locality" content="Au SG">
+<meta property="business:contact_data:region" content="9434">
+<meta property="business:contact_data:postal_code" content="9434">
 <meta property="business:contact_data:country_name" content="Switzerland">
 
-<meta name="google-site-verification" content="tNN6TtO39FlyyJmqITcraNLR48Bw0W21tg9mbhd3EsU" />
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="theme-color" content="#ffffff">
+
+<meta name="google-site-verification" content="...." />
 <link rel="stylesheet" href="css/reset.css" />
 <link rel="stylesheet" href="css/style.css" />
 
