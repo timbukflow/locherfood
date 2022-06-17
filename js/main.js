6ee350f7c5b9cb7f32bdb617183c76d1994d3c8a
Ivo Schwizer <ivoschwizer@gmail.com>
2022-06-17 14:09:00 +0200

    test

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@ $(document).ready(function () {
    });
 
    AOS.init({
-     offset: 250, // offset (in px) from the original trigger point
+     offset: 100, // offset (in px) from the original trigger point
      delay: 0, // values from 0 to 3000, with step 50ms
      duration: 1500, // values from 0 to 3000, with step 50ms
   })
